771646df4349041648b37e9b87a9f18fa764998c
unknown <martino.zanetti@univie.ac.at>
2024-10-08 17:49:32 +0200

    add PM100 usage examples

--- test_fieldmax.py
+++ test_fieldmax.py
@@ -3,6 +3,9 @@ sys.path.append(r"C:\Users\thomas\Documents\GitHub\Fieldmax\pyFieldMaxII-main\fi
 
 import fieldmax  # Now you can import the module
 
+#import visa
+import pyvisa
+from ThorlabsPM100 import ThorlabsPM100
 
 FMII = fieldmax.FieldMax(r'C:\Program Files (x86)\Coherent\FieldMaxII PC\Drivers\Win10\FieldMax2Lib\x64\FieldMax2Lib.dll')
 FMII.openDriver()
@@ -12,20 +15,24 @@ FMII.openDriver()
 FMII.sync()
 
 print(FMII.get_dataPoint())
-print(FMII.get_dataPoint())
-print(FMII.get_dataPoint())
-print(FMII.get_dataPoint())
-print(FMII.get_dataPoint())
-print(FMII.get_dataPoint())
-print(FMII.get_dataPoint())
-print(FMII.get_dataPoint())
-print(FMII.get_dataPoint())
-print(FMII.get_dataPoint())
-print(FMII.get_dataPoint())
-print(FMII.get_dataPoint())
-
-
-
 
 FMII.closeDriver()
 
+
+
+#https://pypi.org/project/ThorlabsPM100/
+rm = pyvisa.ResourceManager('@py')
+#print(rm.list_resources())
+inst = rm.open_resource('USB0::0x1313::0x8072::P2010300::0::INSTR')
+power_meter = ThorlabsPM100(inst=inst)
+inst.timeout = None
+
+power_meter.configure.scalar.power()
+power_meter.sense.correction.wavelength = 1035
+print("Wavelength :", power_meter.sense.correction.wavelength)
+print("Measurement type :", power_meter.getconfigure)
+print("Current value    :", power_meter.read)
+
+print(inst.query("*IDN?"))
+print(rm)
+print(inst)
